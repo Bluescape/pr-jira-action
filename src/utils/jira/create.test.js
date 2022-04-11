7c18fac37cd7d34d477d6576678ab53142788cfd
Landon Martin <landon.martin@bluescape.com>
2022-04-11 12:30:15 -0400

    Add unit tests

---
+++ src/utils/jira/create.test.js
@@ -0,0 +1,82 @@
+const { createTask } = require('./create')
+
+let client
+
+beforeEach(() => {
+  client = {
+    listComponents: jest.fn().mockReturnValue([{
+      id: '12345',
+      name: 'Test',
+      project: 'AD',
+      projectId: 11201,
+      archived: false
+    }
+    ]),
+    addNewIssue: jest.fn().mockReturnValue({
+      key: 'QAA-543'
+    }),
+    getProject: jest.fn().mockReturnValue({ id: '123' })
+  }
+})
+
+test('Creates issue with component when found', async () => {
+  await createTask(client, 'QAA', 'title', { component: 'Test' })
+  expect(client.addNewIssue).toHaveBeenCalledWith({
+    fields: {
+      summary: 'title',
+      description: '',
+      project: {
+        id: '123'
+      },
+      issuetype: {
+        id: '10002'
+      },
+      components: [
+        {
+          id: '12345'
+        }
+      ]
+    }
+  })
+})
+
+test('Creates issue with description when body set', async () => {
+  await createTask(client, 'QAA', 'title', { body: 'Test' })
+  expect(client.addNewIssue).toHaveBeenCalledWith({
+    fields: {
+      summary: 'title',
+      description: 'Test',
+      project: {
+        id: '123'
+      },
+      issuetype: {
+        id: '10002'
+      }
+    }
+  })
+})
+
+test('Creates issue without component when not defined', async () => {
+  await createTask(client, 'QAA', 'title')
+  expect(client.addNewIssue).toHaveBeenCalledWith({
+    fields: {
+      summary: 'title',
+      description: '',
+      project: {
+        id: '123'
+      },
+      issuetype: {
+        id: '10002'
+      }
+    }
+  })
+})
+
+test('Throws when component isnt found', async () => {
+  await expect(async () => await createTask(client, 'QAA', 'title', { component: 'NotReal' })).rejects.toThrow()
+})
+
+test('Throws when key isnt present in issue', async () => {
+  client.addNewIssue = jest.fn().mockReturnValue({})
+  await expect(async () => await createTask(client, 'QAA', 'title')).rejects.toThrow()
+})
