4c09f17946c4b635897cc9e9a0df1cab80b82a85
Landon Martin <55550837+landon-martin@users.noreply.github.com>
2022-04-11 14:56:42 -0400

    Set the component id based on the name (#2)
    * Set the component id based on the name
    
    * Add unit tests
    
    * Newline
    
    * Remove caching from preflight

--- src/index.js
+++ src/index.js
@@ -1,68 +1,24 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const JiraApi = require('jira-client')
+const { createTask } = require('./utils/jira/create')
 
-const issueTypes = {
-  epic: '10000',
-  story: '10001',
-  task: '10002',
-  subTask: '10003',
-  bug: '10004'
-}
+const context = github.context.payload
+const { number, title, body } = context.issue
+const repo = context.repository.name
+const owner = context.repository.owner.login
 
-async function createAndAssignTicket (
-  client,
-  projectId,
-  { summary, description, component }
-) {
-  const issue = await client.addNewIssue({
-    fields: {
-      summary,
-      description,
-      project: {
-        id: projectId
-      },
-      issuetype: {
-        id: issueTypes.task
-      },
-      components: component
-        ? [
-          {
-            id: component
-          }
-        ]
-        : []
-    }
-  })
-  return issue
-}
+const token = core.getInput('github-token')
+const octokit = github.getOctokit(token)
+
+const host = core.getInput('host')
+const username = core.getInput('username')
+const password = core.getInput('password')
+const assignee = core.getInput('assignee')
+const component = core.getInput('component')
+const projectName = core.getInput('project-name')
 
 async function main () {
-  const context = github.context.payload
-  const { number, title, body } = context.issue
-  const repo = context.repository.name
-  const owner = context.repository.owner.login
-
-  const token = core.getInput('github-token')
-  let projectId = core.getInput('project-id')
-  const projectName = core.getInput('project-name')
-
-  const octokit = github.getOctokit(token)
-  if (!projectId && !projectName) {
-    await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: number,
-      body: 'Failed to create JIRA because both project-id and project-name were not defined.'
-    })
-    throw Error('Either project-id or project-name is required')
-  }
-  const host = core.getInput('host')
-  const username = core.getInput('username')
-  const password = core.getInput('password')
-  const assignee = core.getInput('assignee')
-  const component = core.getInput('component')
-
   const client = new JiraApi({
     protocol: 'https',
     host,
@@ -72,38 +28,25 @@ async function main () {
     strictSSL: true
   })
 
-  if (!projectId) {
-    try {
-      projectId = (await client.getProject(projectName.toUpperCase())).id
-    } catch (e) {
-      await octokit.rest.issues.createComment({
-        owner,
-        repo,
-        issue_number: number,
-        body: `Failed to find project id for project with name ${projectName.toUpperCase()}. ${e}`
-      })
-      throw Error(`Failed to find project id for project with name ${projectName.toUpperCase()}. ${e}`)
-    }
-  }
-
-  const issue = await createAndAssignTicket(client, projectId, {
-    summary: title,
-    description: body || '',
-    component
-  })
-  if (!issue.key) {
-    throw Error(JSON.stringify(issue))
-  }
+  const issue = await createTask(client, projectName, title, { component, body })
   console.log(`Ticket created: https://${host}/browse/${issue.key}`)
+
   // adding assignee to the addNewIssue context requires an id instead of an email
   if (assignee) {
     await client.updateAssignee(issue.key, assignee)
     console.log(`> Ticket assigned to ${assignee}`)
     // 31 is In Progress
-    await client.transitionIssue(issue.key, { transition: { id: '31' } })
-    console.log('> Ticket moved to in progress')
+    try {
+      await client.transitionIssue(issue.key, { transition: { id: '31' } })
+      console.log('> Ticket moved to "In Progress"')
+    } catch (e) {
+      console.warn(e)
+      console.warn('> Could not move the ticket to "In Progress", it may not be an option in the workflow')
+    }
   }
+}
 
+main().then(async (issue) => {
   // Update the PR title
   const newTitle = `${issue.key}: ${title}`
   await octokit.rest.pulls.update({
@@ -119,8 +62,12 @@ async function main () {
     issue_number: number,
     body: `JIRA created: https://${host}/browse/${issue.key}`
   })
-}
-
-main().catch((error) => {
+}).catch(async (error) => {
   core.setFailed(error.message)
+  await octokit.rest.issues.createComment({
+    owner,
+    repo,
+    issue_number: number,
+    body: `Failed to create JIRA: ${error.message}`
+  })
 })
---
+++ src/utils/jira/create.js
@@ -0,0 +1,51 @@
+const issueTypes = {
+  epic: '10000',
+  story: '10001',
+  task: '10002',
+  subTask: '10003',
+  bug: '10004'
+}
+
+async function createTask (client, projectName, title, { component, body } = {}) {
+  const projectId = (await client.getProject(projectName.toUpperCase())).id
+
+  let components
+
+  if (component) {
+    // Get the component id
+    const res = await client.listComponents(projectName)
+    const id = res.find(comp => comp.name === component).id
+    if (!id) {
+      throw Error(`Id not found for component '${component}', it may not exist in the project '${projectName}'`)
+    }
+    components = [
+      {
+        id
+      }
+    ]
+  }
+
+  const issue = await client.addNewIssue({
+    fields: {
+      summary: title,
+      description: body || '',
+      project: {
+        id: projectId
+      },
+      issuetype: {
+        id: issueTypes.task
+      },
+      components
+    }
+  })
+
+  if (!issue.key) {
+    throw Error(JSON.stringify(issue))
+  }
+
+  return issue
+}
+
+module.exports = {
+  createTask
+}
