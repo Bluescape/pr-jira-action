ac6de057ff671d3f884449d07617fe1955cfa730
Landon Martin <landon.martin@bluescape.com>
2022-04-12 14:27:50 -0400

    Fix issues with status not being set

---
+++ src/utils/jira/status.test.js
@@ -0,0 +1,28 @@
+const { updateStatusWithName } = require('./status')
+
+let client
+
+beforeEach(() => {
+  jest.clearAllMocks()
+  client = {
+    listStatus: jest.fn().mockReturnValue([{
+      name: 'In Progress',
+      id: '3'
+    }]),
+    transitionIssue: jest.fn().mockReturnValue({})
+  }
+})
+
+test('Gets ID for In progress status', async () => {
+  await updateStatusWithName(client, 'QAA-123', 'In Progress')
+  expect(client.transitionIssue).toHaveBeenCalledWith('QAA-123', { transition: { id: '3' } })
+})
+
+test('Gets ID for non case sensitive status', async () => {
+  await updateStatusWithName(client, 'QAA-123', 'in progrEsS')
+  expect(client.transitionIssue).toHaveBeenCalledWith('QAA-123', { transition: { id: '3' } })
+})
+test('Throws when status id is not found', async () => {
+  await expect(async () => await updateStatusWithName(client, 'QAA-123', 'donso')).rejects.toThrowError("No id found for status of 'donso'")
+  expect(client.transitionIssue).not.toHaveBeenCalled()
+})
